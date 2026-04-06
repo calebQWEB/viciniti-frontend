990868aba7f27c44035e85184e38696904fe0723
RexCaleb89 <c.ochai@rexinsure.com>
2026-04-06 13:23:02 +0100

    Fix

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "https://viciniti-backend-production.up.railway.app",
+  baseURL: "https://viciniti-backend-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
